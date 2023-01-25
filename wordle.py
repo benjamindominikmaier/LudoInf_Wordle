484e275f010c6b489e78d9e105b755ba0d8ba192
Benjamin <benjamindominik.maier@web.de>
2023-01-25 17:09:06 +0000

    Added argparser, reformated code according to PEP8 and PEP257

--- wordle.py
+++ wordle.py
@@ -4,19 +4,13 @@
 import random
 import string
 import operator
+import argparse
 from collections import Counter
 from itertools import chain
 
-DICT = "words_alpha.txt"
-
-ALLOWABLE_CHARACTERS = set(string.ascii_letters)
-#ALLOWABLE_CHARACTERS = set("A", "B", "C", ... "Z", "")
-ALLOWED_ATTEMPTS = 6
-WORD_LENGTH = 5
-
 
 ### Setup ###
-def retrieve_words(path: str, word_length: int=5):
+def retrieve_words(path: str, word_length: int=5) -> list:
     """
     Retrieve a word list with words of given length from a text file
 
@@ -38,9 +32,14 @@ def retrieve_words(path: str, word_length: int=5):
 
 
 ### Word prediction
-def character_probability(words, characters):
+def find_best_word(words: list, characters: list) -> str:
     """
+    Determines best word based on single-character entropy optimization
 
+    :param words: list of all possible words
+    :param characters: list of all allowed characters
+    :returns: word recommendation based on single-character entropy
+    :rtype: str
     """
     result_dict = dict()
     for character in characters:
@@ -54,7 +53,7 @@ def character_probability(words, characters):
             result_dict[character] = abs(counter)
 
     min_value = 9999999999
-    min_word = "Schlecht"
+    min_word = None
 
     for word in words:
         temp_word_value = 0
@@ -66,15 +65,75 @@ def character_probability(words, characters):
         if len(set(word)) < len(word):
             temp_word_value += 5000 * (len(word) - len(set(word)))
         if temp_word_value < min_value:
-            print("Better word found with value ", temp_word_value)
+            # print("Better word found with value ", temp_word_value)
             min_value = temp_word_value
             min_word = word
     #print(min_word, min_value)
-    return min_word
+    if min_word != None:
+        return min_word
+    else:
+        raise ValueError('No word found!')
 
 
-def solve():
+def update_wordlist(words: list, characters: list, user_input: str,
+                    user_response: str):
     """
+    Updates word list based on submitted word and wordle response
+
+    :param words: list of all possible words
+    :param characters: list of all allowed characters
+    :param user_input: word the user submitted to wordle
+    :param user_response: response to word from wordle
+    :returns: updated word- and character-lists
+    :rtype: (list, list)
+    """
+    updated_words = words.copy()
+
+    # check if doubled letters appear in word (user_input)
+    if len(set(user_input)) < len(user_input):
+        print("Warning: Unstable for repeated characters")
+        #raise NotImplementedError
+
+
+    # iterate over all positions of the input word (user_input)
+    for character_index in range(len(user_input)):
+        # if character is correct, remove all words not having the character at
+        # the respective position
+        if user_response[character_index] == "G":
+            for word in words:
+                if user_input[character_index] != word[character_index]:
+                    if word in updated_words:
+                        updated_words.remove(word)
+        elif user_response[character_index] == "Y":
+            # if character appears at a different position, remove all words that
+            # (a) do not contain that character
+            # (b) contain the character at the respective position
+            for word in words:
+                if (user_input[character_index] == word[character_index]) or (user_input[character_index] not in word):
+                    if word in updated_words:
+                        updated_words.remove(word)
+        elif user_response[character_index] == "?":
+            # if character does not appear in the word, remove all words that
+            # contain the respective character and remove the character from the
+            # possible character list
+            if user_input[character_index] in characters:
+                characters.remove(user_input[character_index])
+            for word in words:
+                if user_input[character_index] in word:
+                    if word in updated_words:
+                        updated_words.remove(word)
+        else:
+            raise ValueError(f"Character {user_response[character_index]} not known. Valid options ['G', 'Y', '?']")
+    return updated_words, characters
+
+
+def solve(word_list_path: str="words_alpha.txt",
+          allowed_characters: list=list(set(string.ascii_letters)),
+          word_length: int=5,
+          allowed_attempts: int=6) -> None:
+    """
+    Iterative interactive script to solve wordle
+
     1. obtain possible words
     2. add all characters as possible characters for each position
     3. iterate through attempts
@@ -84,89 +143,55 @@ def solve():
         3.4 remove impossible characters
         3.5 update possible words
 
-    :param:
-    :param:
-    :return:
-    :rtype:
+    :param word_list_path: file path to the word list with all possible words
+    :param allowed_characters: list of all allowed characters (usually A-Z)
+    :param word_length: length of searched word
+    :param allowed_attempts: number of allowed attempts
+    :rtype: None
     """
-    words = retrieve_words(DICT, WORD_LENGTH)
-    characters = ALLOWABLE_CHARACTERS.copy()
-    for attempt in range(1,ALLOWED_ATTEMPTS+1):
+    status = "ongoing"
+    words = retrieve_words(word_list_path, word_length)
+    characters = allowed_characters.copy()
+    for attempt in range(1,allowed_attempts+1):
         print(f"Attempt: {attempt}")
-        print(f"Best Word is: {character_probability(words, characters )}")
-        user_input = input_word(words, WORD_LENGTH)
-        user_response = input_response(WORD_LENGTH)
-        if user_response == "GGGGG":
+        print(f"Best Word is: {find_best_word(words, characters )}")
+        user_input = input_word(words, word_length)
+        user_response = input_response(word_length)
+        if set(user_response) == set("G"):
             print("Congrats!")
+            status = "win"
             break
         words, characters = update_wordlist(words, characters, user_input, user_response)
-    print("Schade!")
-
-
-
-def update_wordlist(words, characters, user_input, user_response):
-    #print(user_input)
-    #print(user_response)
-    updated_words = words.copy()
-
-    for character_index in range(len(user_input)):
-        if user_response[character_index] == "G":
-            # alle wörter auf falschen character überprüfen
-            for word in words:
-                if user_input[character_index] != word[character_index]:
-                    if word in updated_words:
-                        updated_words.remove(word)
-
-        elif user_response[character_index] == "Y":
-            for word in words:
-                if (user_input[character_index] == word[character_index]) or (user_input[character_index] not in word):
-                    if word in updated_words:
-                        updated_words.remove(word)
-
-        elif user_response[character_index] == "?":
-            try:
-                characters.remove(user_input[character_index])
-            except:
-                continue
-
-            for word in words:
-                if user_input[character_index] in word:
-                    if word in updated_words:
-                        updated_words.remove(word)
-
-
-        else:
-            raise ValueError
-
-    return updated_words, characters
-
+    if status == "ongoing":
+        print("Schade!")
+    print("Looking forward to see you tomorrow!")
 
 
 ### User interface ###
-def input_word(word_list: list, word_length: int):
+def input_word(word_list: list, word_length: int) -> str:
     """
     Ask user to input word in command line and return word if the word
     in word list and of right length
 
-    :param word_list:
-    :param word_length:
-    :returns:
-    :rtype:
+    :param word_list: list with all possibble words
+    :param word_length: length of searched word
+    :returns: word the user submitted to wordle
+    :rtype: str
     """
     while True:
         word = input("Input the word you entered> ")
         if len(word) == word_length and word.upper() in word_list:
             return word.upper()
 
-    #word = input("Input the word you entered> ")
-    #if len(word) == word_length and word.upper() in word_list:
-    #    return word
-    #else:
-    #    return input_word(word_list, word_length)
 
-# -> Vortrag Mengenlehre Clemens
-def input_response(word_length):
+def input_response(word_length: int) -> str:
     """
+    Ask user to input response from wordle in command line and return response
+    if the response is of right length and only contains allowed characters
+
+    :param word_length: length of searched word
+    :returns: wordle response to input word entered by user
+    :rtype: str
     """
     print("Type the color-coded reply from Wordle:")
     print("  G for Green")
@@ -186,8 +211,18 @@ def input_response(word_length):
 
 
 ### Benchmark Routine ###
-def benchmark(possible_words, ALLOWABLE_CHARACTERS):
-    best_word = character_probability()
+def benchmark(possible_words, allowed_characters):
+    """
+    Perform automatic benchmark routine to test different solving strategies
+
+    :param possible_words:
+    :param allowed_characters:
+    :param word_length:
+    :param allowed_attempts:
+    :returns:
+    :rtype:
+    """
+    best_word = find_best_word()
     for i in range(10000):
         test_word = random.sample(possible_words, 1)
         for word in possible_words:
@@ -195,16 +230,15 @@ def benchmark(possible_words, ALLOWABLE_CHARACTERS):
                 for word_character in word:
                     pass
 
-### Unit Test ### -> Vortrag (Mentorenteam)
-# 1. Testfunktionen formulieren
-# 2. Schreiben des Programmes
-# =======
-# 1. Code Schreiben
-# 2. Ärgern, weil Code nicht funktioniert
-# 3. Fehler nicht findern, weil keine Tests implementiert
-# 4. Verzweifeln
-# 5. Nun doch Tests schreiben
 
 if __name__ == '__main__':
-    solve()
-    #print(update_wordlist(["hallo","liste","bruno",],ALLOWABLE_CHARACTERS,"clara","?????"))
+    parser = argparse.ArgumentParser(description="Calc identity", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--input_file', type=str, default="words_alpha.txt", help='file path to word list')
+    parser.add_argument('--L', type=int, default=5, help='Word length')
+    parser.add_argument('--N', type=int, default=6, help='Allowed solving attempts')
+    parser.add_argument('--characters', nargs='+', type=int, default=list(set(string.ascii_letters)), help='list of allowed characters')
+
+    args = parser.parse_args()
+
+    solve(word_list_path=args.input_file, allowed_characters=args.characters,
+          word_length=args.L, allowed_attempts=args.N)
